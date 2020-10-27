a2b78def80095a5d28f55922c017304a719ba138
Hurst <304090717@qq.com>
2020-10-27 17:15:03 +0800

    完成了整体的布局-包括登录注册退出等功能

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const bodyParser = require('body-parser')
+var bodyParser = require('body-parser')
 var express = require('express')
 var path = require('path')
+var session = require('express-session')
 var router = require('./router')
 
 var app = express()
@@ -16,8 +17,30 @@ app.set('views', path.join(__dirname, './views/'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+//使用保存在服务器的express-session来保存用户状态信息 
+app.use(session({
+    // 配置加密字符串，它会在原有加密基础之上和这个字符串拼起来去加密
+    // 目的是为了增加安全性，防止客户端恶意伪造
+    secret: 'ironMan',
+    resave: false,
+    saveUninitialized: false // 无论你是否使用 Session ，我都默认直接给你分配一把钥匙
+  }))
+
 // 把路由挂载到app中
 app.use(router)
+
+// 配置处理404的中间件
+app.use((req, res) => {
+    res.render('404.html')
+})
+
+// 配置一个全局错误处理的中间件
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        err_code: 500,
+        message: err.message
+    })
+})
 
 app.listen(5000, () => {
     console.log('running...')
--- router.js
+++ router.js
@@ -1,28 +1,52 @@
 var express = require('express')
 var User = require('./models/user')
-var md5 = require('blueimp-md5')
 var router = express.Router()
+var md5 = require('blueimp-md5')
 
-// 注册主页路由
+// 进入主页
 router.get('/', (req, res) => {
-    res.render('index.html')
+    res.render('index.html', {
+        user: req.session.user
+    })
 })
 
-// 注册登录路由
+// 进入登录页面 
 router.get('/login', (req, res) => {
     res.render('login.html')
 })
 
-// 注册页面--进入注册页面
+// 用户登录
+router.post('/login', (req, res, next) => {
+    var body = req.body
+
+    User.findOne({
+        email: body.email,
+        password: md5(body.password)
+    }, (err, user) => {
+        if (err) return next(err)
+        if (!user) {
+            return res.status(200).json({
+                err_code: 1,
+                message: 'Email or password is invalid.'
+            })
+        }
+        //用户存在，登录成功，通过Session记录登录状态
+        req.session.user = user
+
+        res.status(200).json({
+            err_code: 0,
+            message: 'OK'
+        })
+    })
+})
+
+// 进入注册页面
 router.get('/register', (req, res) => {
     res.render('register.html')
 })
 
-// 注册页面--注册新用户
-// router.post('/posgister', (req, res) => {
-//     res.render('register.html')
-// })
-router.post('/register', function (req, res, next) {
+// 注册新用户
+router.post('/register', (req, res, next) => {
     // 1. 获取表单提交的数据 req.body
     // 2. 操作数据库  判断该用户是否存在 如果已存在，不允许注册；如果不存在，注册新建用户
     // 3. 发送响应
@@ -41,10 +65,8 @@ router.post('/register', function (req, res, next) {
             //   success: false,
             //   message: '服务端错误'
             // })
-            console.log('buxingdela')
             return next(err)
         }
-        // console.log(data)
         if (data) {
             // 邮箱或者昵称已存在
             return res.status(200).json({
@@ -54,11 +76,8 @@ router.post('/register', function (req, res, next) {
             return res.send(`邮箱或者密码已存在，请重试`)
         }
 
-        console.log('ojbk')
-
         // 对密码进行 md5 加密
-          body.password = md5(body.password)
-
+        body.password = md5(body.password)
 
         new User(body).save(function (err, user) {
             if (err) {
@@ -66,7 +85,7 @@ router.post('/register', function (req, res, next) {
             }
 
             // 注册成功，使用 Session 记录用户的登陆状态
-            // req.session.user = user
+            req.session.user = user
 
             // Express 提供了一个响应方法：json
             // 该方法接收一个对象作为参数，它会自动帮你把对象转为字符串再发送给浏览器
@@ -81,4 +100,14 @@ router.post('/register', function (req, res, next) {
     })
 })
 
+//退出 
+router.get('/logout', (req, res) => {
+    //清除登录状态
+    req.session.user = null 
+
+    //重定向到登录页
+    res.redirect('/login')
+})
+
 module.exports = router
+
